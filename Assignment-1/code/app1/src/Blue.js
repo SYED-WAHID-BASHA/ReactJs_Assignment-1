5ec87440f3654e0b501fc3cdb81c6946e6a3eee5
SYED WAHID BASHA <wahidsyedamu@gmail.com>
2022-03-29 22:44:42 +0530

    React-Assignment-1

---
+++ Assignment-1/code/app1/src/Blue.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Blue = () => {
+    return (
+        <div className='bottomContainer'>
+            <div className='bottom'></div>
+            <div className='bottom'></div>
+            <div className='bottom'></div>
+        </div>
+    );
+};
+
+export default Blue;
