7de46867dcfc8ba6dac888a018929883b6d45880
SYED WAHID BASHA <wahidsyedamu@gmail.com>
2022-04-02 15:58:15 +0530

    Assignment-2 submission

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -8,7 +8,26 @@ function App(props) {
     
 return(
   <div className="container">
-  {/* Start your React code here */}
+
+  <div class="sub-container">
+    <img src={employee.profileImg} alt="avatar-logo" />
+    <h1>{employee.name}</h1>
+  </div>
+
+  <div class="sub-container">
+    <p>location</p>
+    <h2>{employee.location}</h2>
+  </div>
+
+  <div class="sub-container">
+    <p>Blood Group</p>
+    <h2>{employee.bloodGroup}</h2>
+  </div>
+
+  <div class="sub-container">
+    <p>age</p>
+    <h2>{employee.age}</h2>
+  </div>
 </div>
 )
 }
