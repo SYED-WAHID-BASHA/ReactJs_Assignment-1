e945c18c026ebd4844498c8e068acf9d8d12c6db
SYED WAHID BASHA <wahidsyedamu@gmail.com>
2022-04-09 11:15:53 +0530

    Assignment-4 submission

--- Assignment-4/code/src/Postview.js
+++ Assignment-4/code/src/Postview.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Postview.css';
+import Post from './component/Post';
+import Header from './component/Haeder';
 
 
 const Postview=()=> {
+
+  const[data,usedata]=useState([])
+
+  const getData=async()=>{
+    let response=await fetch("http://localhost:3004/user");
+    let stres=await response.json();
+    usedata(stres);
+
+  }
+
+    useEffect(() =>{getData()},[]);
+    console.log(data);
+
+  
   return (
     <div className="site-container">
+    <Header />
+      {data.map((ele, idx)=>{
+        return(
+
+          <Post
+          key={idx}
+          name={ele.name}
+          location={ele.location} 
+          likes={ele.likes}
+          desc={ele.description}
+          image={ele.PostImage}
+          date={ele.date}/>
+
+        )
+      } )}
+      
     </div>
   );
 }
 
 export default Postview;
+
+
+
+// const [data, setData] = useState([]);
+
+//   useEffect(() => {
+//     fetch("http://localhost:3001/links/")
+//       .then(response => response.json())
+//       .then(data => setData(data));
+//   }, []); // << super important array
+
+//   return props.render(data)
---
+++ Assignment-4/code/src/component/Haeder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header(){
+    return(
+        <div className='Header'>
+            <div className='header-content'>
+                InstaClone </div>
+            <div className='header-image'>
+                <img src="https://www.nicepng.com/png/detail/172-1726326_svg-free-download-onlinewebfonts-camera-icon-svg-free.png" alt=""/></div>
+        </div>
+    )
+}
